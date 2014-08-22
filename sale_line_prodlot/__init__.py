522236ead0bf5c605bd3404156c621dd063ec5c3
gwsilva <gideoni.silva@omnes.net.br>
2014-08-22 11:08:55 -0300

    Update __init__.py

--- sale_line_prodlot/__init__.py
+++ sale_line_prodlot/__init__.py
@@ -20,8 +20,6 @@
 ##############################################################################
 
 import sale
-import procurement
-import wizard
 import stock
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
